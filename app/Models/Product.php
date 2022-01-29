24af1daf45058ad80ff2ef90098dbafcea551152
Ian <ianstigli@hotmail.com>
2022-01-29 02:48:52 -0300

    database creation using Models, migrations, seeds and factories

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillables = [
+        'category_id',
+        'name',
+        'description',
+        'exclusive'
+    ];
+
+    protected $dates = [
+      'deleted_at'  
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+}

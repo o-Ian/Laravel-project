cee97e51eb39e66276e98615ed7f35bb7376ae7c
Ian <ianstigli@hotmail.com>
2022-01-29 04:26:58 -0300

    release/1.3.0

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Contact extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'email',

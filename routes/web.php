b68c4829d3009b2779a122083e4354571027ad9b
Ian <ianstigli@hotmail.com>
2022-01-29 00:49:30 -0300

    creation of basics routes, views and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+Route::get('produtos', [CategoryController::class, 'index']);
+Route::get('produtos/{slug}', [CategoryController::class, 'show']);
+
+Route::get('blog', BlogController::class);
+
+Route::view('sobre', 'about/index');
+
+Route::get('contato', [ContactController::class, 'index']);
+Route::post('contato', [ContactController::class, 'form']);
